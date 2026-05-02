ae69da7d8d07495b536e8bfa6976641a3991c467
Abbos Sharifboyev <abbossharifboyevv@gmail.com>
2026-05-03 00:16:28 +0500

    reformat code and optimize improts

--- src/main/java/uz/pdp/smartinventory/config/CustomSecurityService.java
+++ src/main/java/uz/pdp/smartinventory/config/CustomSecurityService.java
@@ -1,7 +1,6 @@
 package uz.pdp.smartinventory.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
@@ -13,14 +12,14 @@ public class CustomSecurityService {
 
     private final UserRepository userRepository;
 
-    public boolean hasPermission(String permission){
+    public boolean hasPermission(String permission) {
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-        if (authentication == null || !authentication.isAuthenticated()){
+        if (authentication == null || !authentication.isAuthenticated()) {
             return false;
         }
         String username = authentication.getName();
-        return userRepository.hasPermission(username,permission);
+        return userRepository.hasPermission(username, permission);
     }
 }
