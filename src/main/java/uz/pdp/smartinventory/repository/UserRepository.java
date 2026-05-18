ed7a42d4434e8c4444565bd1bd6c7ba3436ddab0
Abbos Sharifboyev <abbossharifboyevv@gmail.com>
2026-05-18 10:22:50 +0500

    StockMovement with OrderService

--- src/main/java/uz/pdp/smartinventory/repository/UserRepository.java
+++ src/main/java/uz/pdp/smartinventory/repository/UserRepository.java
@@ -1,17 +1,12 @@
 package uz.pdp.smartinventory.repository;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.security.core.parameters.P;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 import uz.pdp.smartinventory.model.domain.Users;
-import uz.pdp.smartinventory.model.dto.UserDto;
 import uz.pdp.smartinventory.model.enums.Role;
 
-import java.security.SecureRandom;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
